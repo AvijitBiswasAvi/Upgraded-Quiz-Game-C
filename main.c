7b95b71489f6b2b8bfd67376d8bf8a8d4fecf546
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-18 13:06:12 +0600

    upgrade quiz game

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ int main() {
     printf("|    WELCOME TO THE QUIZ GAME    |\n");
     printf("|--------------------------------|\n");
     printf("| 1. Start the game              |\n");
+    printf("| 2. Rules                       |\n");
     
+
     return 0;
 }
