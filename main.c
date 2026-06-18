4df8e8f118f46b2cb31f10b856617710cf638134
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-18 13:14:29 +0600

    upgarde quiz game

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 
 int main() {
     
+    int starting_choice;
+
+    printf("\n");
     printf("|================================|\n");
     printf("|    WELCOME TO THE QUIZ GAME    |\n");
     printf("|--------------------------------|\n");
     printf("| 1. Start the game              |\n");
     printf("| 2. Rules                       |\n");
-    
+    printf("|================================|\n");
+    printf("\nEnter your choice = ");
 
     return 0;
 }
