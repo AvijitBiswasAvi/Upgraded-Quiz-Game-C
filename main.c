e656921db8a805c780b69eded5ab7c943b48ca19
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-20 11:33:26 +0600

    upgrade quiz game

--- main.c
+++ main.c
@@ -13,8 +13,21 @@ int main() {
     printf("| 1. Start the game              |\n");
     printf("| 2. Rules                       |\n");
     printf("|================================|\n");
-    printf("\nEnter your choice = ");  
-    
 
+    printf("\nEnter your choice = ");  
+    scanf("%d", &starting_choice);
+
+    if (starting_choice = 1)
+    {
+        printf("|=============================|\n");
+        printf("|      CHOOSE DIFICULTY       |\n");
+        printf("|-----------------------------|\n");
+        printf("| 1. Easy ( 5 Questions )     |\n");
+        printf("| 2. Medium ( 10 Questions )  |\n");
+        printf("| 3. Hard ( 15 Questions )    |\n");
+        printf("|=============================|\n");
+    }
+    
+    
     return 0;
 }
