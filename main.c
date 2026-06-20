1eae771f2e120c7bf5f8c2ace5b4f91fc319b193
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-20 12:43:30 +0600

    upgrade quiz game

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main()
             strcpy(game[4].answers, "C");
         }
 
-        if (dificulty_choice == 2)
+       else if (dificulty_choice == 2)
         {
             int max_questions = 10;
             Quiz game[10];
@@ -101,6 +101,85 @@ int main()
             strcpy(game[7].answers, "B");
             strcpy(game[8].answers, "C");
             strcpy(game[9].answers, "B");
+        }
+
+       else if (dificulty_choice == 3)
+        {
+            int max_questions = 15;
+            Quiz game[15];
+
+            strcpy(game[0].questions, "Which country was the first to give women the right to vote?");
+            strcpy(game[1].questions, "What is the term for the interest rate at which central banks lend money to commercial banks?");
+            strcpy(game[2].questions, "In 'Money Heist,' what is the real name of 'The Professor'?");
+            strcpy(game[3].questions, "Which ocean is the deepest in the world?");
+            strcpy(game[4].questions, "Which player has won the most Ballon d'Or awards as of 2024?");
+            strcpy(game[5].questions, "Which Indian state has the longest coastline?");
+            strcpy(game[6].questions, "What does 'blockchain' primarily provide in cryptocurrency systems?");
+            strcpy(game[7].questions, "In 'Death Note,' what is the human name of the detective known as 'L'?");
+            strcpy(game[8].questions, "Which country has won the most FIFA World Cup titles?");
+            strcpy(game[9].questions, "What is the smallest country in the world by land area?");
+            strcpy(game[10].questions, "Which company's stock ticker symbol is 'TSLA'?");
+            strcpy(game[11].questions, "In 'Demon Slayer,' what is the name of Tanjiro's signature breathing technique?");
+            strcpy(game[12].questions, "Which treaty officially ended World War I?");
+            strcpy(game[13].questions, "What does 'API' stand for in technology?");
+            strcpy(game[14].questions, "In chess, what is the only piece that can jump over other pieces?");
+
+            strcpy(game[0].options, "A) USA  B) New Zealand  C) UK  D) Sweden");
+            strcpy(game[1].options, "A) Inflation Rate  B) Exchange Rate  C) Repo Rate  D) Prime Rate");
+            strcpy(game[2].options, "A) Andres de Fonollosa  B) Sergio Marquina  C) Alvaro Morte  D) Raul Arevalo");
+            strcpy(game[3].options, "A) Atlantic Ocean  B) Indian Ocean  C) Arctic Ocean  D) Pacific Ocean");
+            strcpy(game[4].options, "A) Cristiano Ronaldo  B) Zinedine Zidane  C) Lionel Messi  D) Ronaldinho");
+            strcpy(game[5].options, "A) Kerala  B) Tamil Nadu  C) Gujarat  D) Maharashtra");
+            strcpy(game[6].options, "A) Faster internet  B) Decentralized, tamper-proof ledger  C) Free transactions  D) Government control");
+            strcpy(game[7].options, "A) Light Yagami  B) Near  C) L Lawliet  D) Mello");
+            strcpy(game[8].options, "A) Germany  B) Argentina  C) Brazil  D) Italy");
+            strcpy(game[9].options, "A) Monaco  B) San Marino  C) Vatican City  D) Liechtenstein");
+            strcpy(game[10].options, "A) Toyota  B) Tesla  C) Tata Motors  D) Texas Instruments");
+            strcpy(game[11].options, "A) Thunder Breathing  B) Water Breathing  C) Flame Breathing  D) Wind Breathing");
+            strcpy(game[12].options, "A) Treaty of Paris  B) Treaty of Versailles  C) Treaty of Vienna  D) Treaty of Lausanne");
+            strcpy(game[13].options, "A) Application Programming Interface  B) Advanced Programming Index  C) Automated Process Integration  D) Application Process Identifier");
+            strcpy(game[14].options, "A) Bishop  B) Rook  C) Knight  D) Queen");
+
+            strcpy(game[0].answers, "B");
+            strcpy(game[1].answers, "C");
+            strcpy(game[2].answers, "B");
+            strcpy(game[3].answers, "D");
+            strcpy(game[4].answers, "C");
+            strcpy(game[5].answers, "C");
+            strcpy(game[6].answers, "B");
+            strcpy(game[7].answers, "C");
+            strcpy(game[8].answers, "C");
+            strcpy(game[9].answers, "C");
+            strcpy(game[10].answers, "B");
+            strcpy(game[11].answers, "B");
+            strcpy(game[12].answers, "B");
+            strcpy(game[13].answers, "A");
+            strcpy(game[14].answers, "C");
+        }
+        
+        else
+        {
+            printf("\n--- Invalid choice ! Defaulting to Easy mode ---\n");
+            int max_questions = 5;
+            Quiz game[5];
+
+            strcpy(game[0].questions, "Which planet is known as the Red Planet?");
+            strcpy(game[1].questions, "How many continents are there on Earth?");
+            strcpy(game[2].questions, "Which company made the iPhone?");
+            strcpy(game[3].questions, "In which anime does the main character want to become the 'King of Pirates'?");
+            strcpy(game[4].questions, "How many players are on a football (soccer) team on the field?");
+
+            strcpy(game[0].options, "A) Jupiter  B) Venus  C) Mars  D) Saturn");
+            strcpy(game[1].options, "A) 5  B) 6  C) 8  D) 7");
+            strcpy(game[2].options, "A) Samsung  B) Apple  C) Sony  D) Nokia");
+            strcpy(game[3].options, "A) Naruto  B) Bleach  C) One Piece  D) Dragon Ball Z");
+            strcpy(game[4].options, "A) 10  B) 12  C) 11  D) 9");
+
+            strcpy(game[0].answers, "C");
+            strcpy(game[1].answers, "D");
+            strcpy(game[2].answers, "B");
+            strcpy(game[3].answers, "C");
+            strcpy(game[4].answers, "C");
         }
     }
 
