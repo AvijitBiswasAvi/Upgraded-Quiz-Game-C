f8fe5c58b59502dd8b4c99718a496c1bf6df0e72
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-21 11:31:56 +0600

    upgrade quiz game

--- main.c
+++ main.c
@@ -203,8 +203,33 @@ int main()
                 wrong_ans++;
             }
         }
+
+        int score = (correct_ans * 100) / max_questions;
+        char grade;
+
+        if (score >= 90 && score <= 100)
+        {
+            grade = 'A';
+        }
+        else if (score >= 80 && score <= 89)
+        {
+            grade = 'B';
+        }
+        else if (score >= 70 && score <= 79)
+        {
+            grade = 'C';
+        }
+        else if (score >= 60 && score <= 69)
+        {
+            grade = 'D';
+        }
+        else
+        {
+            grade = 'F';
+        }
+        
     }   
-    
+
 
     return 0;
 }
